8bb87ddc3c172cd61014c8648b85c3d50c9ea9ad
Nicolas Mendoza <nicolas.mendozaion@gmail.com>
2024-05-14 13:51:35 -0300

    added project names and more decotaion on the inputs

--- bin/index.js
+++ bin/index.js
@@ -3,12 +3,44 @@
 import { program } from "commander";
 import chalk from "chalk";
 import inquirer from "inquirer";
-// import CustomPrompt from "./CustomPrompt";
 import ora from "ora";
 
-program.version("1.0.0").description("Create tension project");
+import names from '../src/utils/projectNames.json' assert { type: 'json' };
 
+
+const spinnerBar = {
+  interval: 80, // Optional
+  frames: [
+    chalk.hex('#68217a')('██████'), 
+    chalk.hex('#5B3864')('█') + chalk.hex('#68217a')('█████'), 
+    chalk.hex('#554359')('█') + chalk.hex('#5B3864')('█') + chalk.hex('#68217a')('████'), 
+    chalk.hex('#4E4E4D')('█') + chalk.hex('#554359')('█') + chalk.hex('#5B3864')('█') + chalk.hex('#68217a')('███'), 
+    chalk.hex('#445F3D')('█') + chalk.hex('#4E4E4D')('█') + chalk.hex('#554359')('█') + chalk.hex('#5B3864')('█') + chalk.hex('#68217a')('██'),
+    chalk.hex('#3A6F2C')('█') + chalk.hex('#445F3D')('█') + chalk.hex('#4E4E4D')('█') + chalk.hex('#554359')('█') + chalk.hex('#5B3864')('█') + chalk.hex('#68217a')('█'),
+    chalk.hex('#337A20')('█') + chalk.hex('#3A6F2C')('█') + chalk.hex('#4E4E4D')('█') + chalk.hex('#554359')('█') + chalk.hex('#5B3864')('█') + chalk.hex('#68217a')('█'),
+    chalk.hex('#337A20')('██') + chalk.hex('#3A6F2C')('█') + chalk.hex('#554359')('█') + chalk.hex('#4E4E4D')('█') + chalk.hex('#554359')('█'),
+    chalk.hex('#337A20')('███') + chalk.hex('#3A6F2C')('█') + chalk.hex('#4E4E4D')('█') + chalk.hex('#554359')('█'),
+    chalk.hex('#337A20')('████') + chalk.hex('#3A6F2C')('█') + chalk.hex('#4E4E4D')('█'),
+    chalk.hex('#337A20')('█████') + chalk.hex('#3A6F2C')('█'),
+    chalk.hex('#337A20')('██████'),
+    chalk.hex('#3A6F2C')('█') + chalk.hex('#337A20')('█████'),
+    chalk.hex('#445F3D')('█') + chalk.hex('#3A6F2C')('█') + chalk.hex('#337A20')('████'),
+    chalk.hex('#4E4E4D')('█') + chalk.hex('#445F3D')('█') + chalk.hex('#3A6F2C')('█') + chalk.hex('#337A20')('███'),
+    chalk.hex('#554359')('█') + chalk.hex('#4E4E4D')('█') + chalk.hex('#445F3D')('█') + chalk.hex('#3A6F2C')('█') + chalk.hex('#337A20')('██'),
+    chalk.hex('#5B3864')('█') + chalk.hex('#554359')('█') + chalk.hex('#4E4E4D')('█') + chalk.hex('#445F3D')('█') + chalk.hex('#3A6F2C')('█') + chalk.hex('#337A20')('█'),
+    chalk.hex('#68217A')('█') + chalk.hex('#5B3864')('█') + chalk.hex('#554359')('█') + chalk.hex('#4E4E4D')('█') + chalk.hex('#445F3D')('█') + chalk.hex('#3A6F2C')('█'),
+    chalk.hex('#68217A')('██') + chalk.hex('#5B3864')('█') + chalk.hex('#554359')('█') + chalk.hex('#4E4E4D')('█') + chalk.hex('#445F3D')('█'),
+    chalk.hex('#68217A')('███') + chalk.hex('#5B3864')('█') + chalk.hex('#554359')('█') + chalk.hex('#4E4E4D')('█'),
+    chalk.hex('#68217A')('████') + chalk.hex('#5B3864')('█') + chalk.hex('#554359')('█'),
+    chalk.hex('#68217A')('█████') + chalk.hex('#5B3864')('█')]
+};
 // inquirer.registerPrompt('custom', CustomPrompt);
+
+
+program.version("1.0.0").description("Create tension project");
+console.log("\n" + chalk.black.bold.bgHex('#ffd31b')(' TENSION '))
+
+
 program.action(() => {
   inquirer
     .prompt([
@@ -16,34 +48,38 @@ program.action(() => {
         type: "input",
         name: "name",
         message: "Where should we create your new project?",
-        default: "./holamundo",
-        prefix: "   " + chalk.white.bgHex('#C66394')(' DIR ') + " "
+        default: "project-"+names[Math.floor(Math.random() * names.length)],
+        prefix: "\n   " + chalk.white.bgHex('#68217a')(' DIR ') + " "
       },
       {
         type: "list",
         name: "template",
         message: "Choose a template:",
         choices: ["Include sample files", "Empty"],
-        prefix: "   " + chalk.white.bgHex('#C66394')(' tmpl ') + " "
+        prefix: "\n   " + chalk.white.bgHex('#68217a')(' tmpl ') + " "
       },
       {
         type: "list",
         name: "js",
         message: "Do you plan to write TypeScript?",
         choices: ["Yes", "No"],
-        prefix: "   " + chalk.white.bgHex('#C66394')(' js ') + " "
+        prefix: "\n   " + chalk.white.bgHex('#68217a')(' js ') + " "
       },
       {
         type: "list",
         name: "git",
         message: "Initialize a new git repository? (optional)",
         choices: ["Yes", "No"],
-        prefix: "   " + chalk.white.bgHex('#C66394')(' git ') + " "
+        prefix: "\n   " + chalk.white.bgHex('#68217a')(' git ') + " "
       },
     ])
     .then((result) => {
-      const spinner = ora(`Doing ${result.choice}...`).start(); // Start the spinner
-
+      const spinner = ora({
+        text: `Doing ${result.name}...`,
+        spinner: spinnerBar,
+        prefixText: "\n"
+      });
+      spinner.start();// Start the spinner
       setTimeout(() => {
         spinner.succeed(chalk.green("Done!"));
       }, 3000);
